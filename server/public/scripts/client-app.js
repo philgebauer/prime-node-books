133c312334da9c336e57b1937bd017154960c06f
Antoinette Smith <antoinette@primeacademy.io>
2016-08-17 12:52:03 -0500

    minor formatting changes

--- server/public/scripts/client-app.js
+++ server/public/scripts/client-app.js
@@ -1,38 +1,36 @@
-$(document).ready(function() {
+$(document).ready(function () {
     getBooks();
 
     // add a book
     $('#book-submit').on('click', postBook);
     $('#book-list').on('click', '.update', putBook);
-});
+  });
 /**
  * Retrieve books from server and append to DOM
  */
 function getBooks() {
-    $.ajax({
+  $.ajax({
         type: 'GET',
         url: '/books',
-        success: function(books) {
+        success: function (books) {
             console.log('GET /books returns:', books);
-            books.forEach(function(book) {
+            books.forEach(function (book) {
                 var $el = $('<div></div>');
 
                 var bookProperties = ['title', 'author', 'published', 'genre'];
 
-                bookProperties.forEach(function(property) {
+                bookProperties.forEach(function (property) {
                     var inputType = 'text';
                     if (property == 'published') {
-                        //inputType = 'date';
-                        //get strings for month/day/year
-                        book[property] = new Date(book[property]);      
-                        var month = book[property].getUTCMonth(book[property]) + 1; //months from 1-12
-                                    
-                        var day = book[property].getUTCDate(book[property]);            
-                        var year = book[property].getUTCFullYear(book[property]);
+                      //inputType = 'date';
+                      //get strings for month/day/year
+                      book[property] = new Date(book[property]);
+                      var month = book[property].getUTCMonth(book[property]) + 1; //months from 1-12                      
+                      var day = book[property].getUTCDate(book[property]);
+                      var year = book[property].getUTCFullYear(book[property]);
 
-                                     //catcatcanate into one string month/day/year and set to book.published as text
-                                    
-                        book[property] = month + "/" + day + "/" + year;
+                      //concatenate into one string month/day/year and set to book.published as text
+                      book[property] = month + "/" + day + "/" + year;
                     }
 
                     console.log('properties', book[property]);
