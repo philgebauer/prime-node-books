157f45a036dff55f8a43fc417df83ff59c0555ee
Kris Szafranski <kdszafranski@gmail.com>
2016-11-15 11:23:08 -0600

    rewind to lecture start

--- server/app.js
+++ server/app.js
@@ -4,17 +4,16 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 // Our routes
-var books = require('./routes/books');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/books', books);
 
 // Catchall route
-app.get('/*', function (req, res) {
-  var file = req.params[0] || '/views/index.html';
-  res.sendFile(path.join(__dirname, './public', file));
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve('./server/public/views/index.html'));
 });
+
+app.use(express.static('./server/public'));
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function () {
